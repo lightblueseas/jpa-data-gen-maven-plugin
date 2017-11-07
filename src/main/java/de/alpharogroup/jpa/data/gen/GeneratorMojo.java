969db12566bf0c571f9a2c9662dd9fa36ffd2996
Asterios Raptis <asterios.raptis@gmx.net>
2017-11-07 23:24:48 +0100

    changed parent project

--- src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
+++ src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
@@ -5,10 +5,15 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Mojo(name = "jpa-generator")
 public class GeneratorMojo extends AbstractMojo
 {
 
+	@Getter
+	@Setter
 	@Parameter(property = "parent", defaultValue = "default-parent")
 	private String parent;
 
@@ -16,16 +21,6 @@ public class GeneratorMojo extends AbstractMojo
 	public void execute() throws MojoExecutionException
 	{
 		getLog().info("Hello " + parent);
-	}
-
-	public String getParent()
-	{
-		return parent;
-	}
-
-	public void setParent(final String parent)
-	{
-		this.parent = parent;
 	}
 
 }

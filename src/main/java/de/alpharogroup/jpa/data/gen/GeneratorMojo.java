73ae2a970d3ef104f5cfb99148b3473d06d91228
Asterios Raptis <asterios.raptis@gmx.net>
2017-11-10 12:32:25 +0100

    extended generation process

--- src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
+++ src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
@@ -5,22 +5,57 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Mojo(name = "jpa-generator")
+@FieldDefaults(level=AccessLevel.PRIVATE)
+@Getter
+@Setter
 public class GeneratorMojo extends AbstractMojo
 {
 
-	@Getter
-	@Setter
-	@Parameter(property = "parent", defaultValue = "default-parent")
-	private String parent;
+	@Parameter(property = "parentName", required = true)
+	String parentName;
+
+	@Parameter(property = "dataProjectName")
+	String dataProjectName;
+
+	@Parameter(property = "dataProjectName")
+	String dataProjectVersion;
+
+	@Parameter(property = "basePackageName", required = true)
+	String basePackageName;
+
+	@Parameter(property = "persistenceunitName", required = true)
+	String persistenceunitName;
+
+	@Parameter(property = "dataProjectParentVersion")
+	String dataProjectParentVersion;
 
 	@Override
 	public void execute() throws MojoExecutionException
 	{
-		getLog().info("Hello " + parent);
+		getLog().info("Hello " + parentName);
+		getLog().info("Hello " + dataProjectName);
+		getLog().info("Hello " + dataProjectVersion);
+		getLog().info("Hello " + basePackageName);
+		getLog().info("Hello " + dataProjectParentVersion);
+		getLog().info("Hello " + persistenceunitName);
+
+//		try
+//		{
+//			GeneratorExtensions.generatePomFiles();
+//			GeneratorExtensions.generateRepositoryClasses(false);
+//		}
+//		catch (final Exception e)
+//		{
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			throw new MojoExecutionException("", e);
+//		}
 	}
 
 }

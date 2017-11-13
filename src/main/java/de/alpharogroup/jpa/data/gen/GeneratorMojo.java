5b45766d641421cc74dab42d3df8f2a2400b1237
Asterios Raptis <asterios.raptis@gmx.net>
2017-11-13 12:44:28 +0100

    update of absoluteProjectPath Parameter

--- src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
+++ src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
@@ -5,6 +5,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import de.alpharogroup.gen.src.generator.GeneratorExtensions;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,49 +18,53 @@ import lombok.experimental.FieldDefaults;
 public class GeneratorMojo extends AbstractMojo
 {
 
-	@Parameter(property = "parentName", required = true)
-	String parentName;
-
-	@Parameter(property = "dataProjectName")
-	String dataProjectName;
-
-	@Parameter(property = "dataProjectName")
-	String dataProjectVersion;
+	@Parameter(property = "absoluteProjectPath", defaultValue=".")
+	String absoluteProjectPath;
 
 	@Parameter(property = "basePackageName", required = true)
 	String basePackageName;
 
-	@Parameter(property = "persistenceunitName", required = true)
-	String persistenceunitName;
-
-	@Parameter(property = "password", required = true)
-	String password;
+	@Parameter(property = "dataProjectName")
+	String dataProjectName;
 
 	@Parameter(property = "dataProjectParentVersion")
 	String dataProjectParentVersion;
 
+	@Parameter(property = "dataProjectName")
+	String dataProjectVersion;
+
+	@Parameter(property = "parentName", required = true)
+	String parentName;
+
+	@Parameter(property = "password", required = true)
+	String password;
+
+	@Parameter(property = "persistenceunitName", required = true)
+	String persistenceunitName;
+
 	@Override
 	public void execute() throws MojoExecutionException
 	{
-		getLog().info("Hello " + parentName);
-		getLog().info("Hello " + dataProjectName);
-		getLog().info("Hello " + dataProjectVersion);
-		getLog().info("Hello " + basePackageName);
-		getLog().info("Hello " + dataProjectParentVersion);
-		getLog().info("Hello " + persistenceunitName);
-		getLog().info("Hello " + password);
+		getLog().info("Parameter 'parentName' is:" + parentName);
+		getLog().info("Parameter 'dataProjectName' is:" + dataProjectName);
+		getLog().info("Parameter 'dataProjectVersion' is:" + dataProjectVersion);
+		getLog().info("Parameter 'basePackageName' is:" + basePackageName);
+		getLog().info("Parameter 'dataProjectParentVersion' is:" + dataProjectParentVersion);
+		getLog().info("Parameter 'persistenceunitName' is:" + persistenceunitName);
+		getLog().info("Parameter 'password' is:" + password);
+		getLog().info("Parameter 'absoluteProjectPath' is:" + absoluteProjectPath);
 
-//		try
-//		{
-//			GeneratorExtensions.generatePomFiles();
-//			GeneratorExtensions.generateRepositoryClasses(false);
-//		}
-//		catch (final Exception e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			throw new MojoExecutionException("", e);
-//		}
+		try
+		{
+			GeneratorExtensions.generatePomFiles();
+			GeneratorExtensions.generateRepositoryClasses(true);
+		}
+		catch (final Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new MojoExecutionException("", e);
+		}
 	}
 
 }

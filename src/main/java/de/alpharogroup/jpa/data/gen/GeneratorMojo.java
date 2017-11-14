497a584d63a258210ea68c390ce4ded9d592f4b8
Asterios Raptis <asterios.raptis@gmx.net>
2017-11-14 08:36:37 +0100

    extended generation process

--- src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
+++ src/main/java/de/alpharogroup/jpa/data/gen/GeneratorMojo.java
@@ -9,12 +9,16 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
 
 import de.alpharogroup.gen.src.generator.GeneratorExtensions;
 import de.alpharogroup.gen.src.model.PomGenerationModelBean;
@@ -32,8 +36,6 @@ import lombok.experimental.FieldDefaults;
 public class GeneratorMojo extends AbstractMojo
 {
 
-	@Parameter(defaultValue = "${project}", required = true, readonly = true)
-	MavenProject project;
 
 	@Parameter(property = "absoluteProjectPath", defaultValue = ".")
 	String absoluteProjectPath;
@@ -59,6 +61,35 @@ public class GeneratorMojo extends AbstractMojo
 	@Parameter(property = "persistenceunitName", required = true)
 	String persistenceunitName;
 
+	@Component
+	PluginDescriptor descriptor;
+
+	// @Parameter(defaultValue = "${project}", required = true, readonly = true)
+	@Component
+	MavenProject project;
+
+	protected void addDependenciesToClasspath(final String artifactId)
+	{
+		for (final Artifact artifact :
+			project.getDependencyArtifacts())
+		{
+			if (artifact.getArtifactId().equals(artifactId))
+			{
+				try
+				{
+					final URL url = artifact.getFile().toURI().toURL();
+					final ClassRealm realm = descriptor.getClassRealm();
+					realm.addURL(url);
+				}
+				catch (final MalformedURLException e)
+				{
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+
+
 	@Override
 	public void execute() throws MojoExecutionException
 	{
@@ -78,17 +109,19 @@ public class GeneratorMojo extends AbstractMojo
 
 			elements.addAll(project.getTestClasspathElements());
 			elements.addAll(project.getRuntimeClasspathElements());
-			 elements.addAll(project.getCompileClasspathElements());
-			 elements.addAll(project.getSystemClasspathElements());
-			for (final String element : elements)
+			elements.addAll(project.getCompileClasspathElements());
+			elements.addAll(project.getSystemClasspathElements());
+			final URL[] runtimeUrls = new URL[elements.size()];
+			for (int i = 0; i < elements.size(); i++)
 			{
-				urls.add(new File(element).toURI().toURL());
+				final String element = elements.get(i);
+				runtimeUrls[i] = new File(element).toURI().toURL();
 			}
+			final URLClassLoader newLoader = new URLClassLoader(runtimeUrls,
+				Thread.currentThread().getContextClassLoader());
 
-			final ClassLoader contextClassLoader = URLClassLoader.newInstance(
-				urls.toArray(new URL[0]), Thread.currentThread().getContextClassLoader());
+			Thread.currentThread().setContextClassLoader(newLoader);
 
-			Thread.currentThread().setContextClassLoader(contextClassLoader);
 
 		}
 		catch (final DependencyResolutionRequiredException e)
